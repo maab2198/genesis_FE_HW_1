7c6c61aaebe629fa4673eaed8d570256f9241d4a
maab2198 <maab2198@gmail.com>
2021-11-17 20:49:00 +0200

    feat(ui): section wrapper, fix avatar

--- src/components/UI/Avatar.jsx
+++ src/components/UI/Avatar.jsx
@@ -1,21 +1,20 @@
 import React from "react"
 import styles from "./Avatar.module.css"
 
-export const Avatar = ({ author }) => {
+export const Avatar = ({ link, size, src }) => {
+  let s = size == "small" ? "60px" : "120px"
+
   return (
-    <div className={styles.avatar__container}>
-      <a
-        className="avatar-anchor tiktok-1tizhk9 e1eulw5o4"
-        href={"/@" + author.name}
-      >
-        <span className={styles.avatar__wrapper}>
-          <img loading="lazy" src={author.avatar} className={styles.avatar} />
-        </span>
-      </a>
-      <a className="tiktok" href={"/@" + author.name}>
-        <h3 className="tiktok">{author.name}</h3>
-        <h4 className="tiktok">{author.nickName}</h4>
-      </a>
-    </div>
+    <a className="avatar-anchor" href={"user/" + link}>
+      <span className={styles.avatar__wrapper} style={{ width: s, height: s }}>
+        <img
+          loading="lazy"
+          src={src}
+          className={styles.avatar}
+          width={s}
+          height={s}
+        />
+      </span>
+    </a>
   )
 }
