7ed0ef088a9e2ee5531f8fae20cffe5cbf6670b8
maab2198 <maab2198@gmail.com>
2021-11-17 23:19:17 +0200

    feat(user): add UserFeed to Page

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,8 +1,10 @@
 import React from "react"
-import Layout from "../components/Layout/Layout"
-import UserInfo from "../components/User/UserInfo"
 import SectionWrapper from "../components/UI/SectionWrapper"
 import { useParams } from "react-router-dom"
+
+import Layout from "../components/Layout/Layout"
+import UserInfo from "../components/User/UserInfo"
+import UserFeed from "../components/User/UserFeed"
 
 export default function UserPage() {
   const params = useParams()
@@ -12,6 +14,7 @@ export default function UserPage() {
     <Layout>
       <SectionWrapper>
         <UserInfo userId={userId} />
+        <UserFeed userId={userId} />
       </SectionWrapper>
     </Layout>
   )
