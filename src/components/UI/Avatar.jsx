016030ecf73775014a6f02fe438d27231a6869ab
maab2198 <maab2198@gmail.com>
2021-11-17 23:18:26 +0200

    fix(avatar): bugfix

--- src/components/UI/Avatar.jsx
+++ src/components/UI/Avatar.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import styles from "./Avatar.module.css"
 
 export const Avatar = ({ link, size, src }) => {
-  let s = size == "small" ? "60px" : "120px"
+  let s = size === "small" ? "60px" : "120px"
 
   return (
     <a className="avatar-anchor" href={"user/" + link}>
