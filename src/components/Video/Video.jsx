6f1d8868390d5bc9c783e6b264f3cc0a48a18a52
maab2198 <maab2198@gmail.com>
2021-11-17 23:18:49 +0200

    feat(video): component

---
+++ src/components/Video/Video.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const Video = (props) => {
+const [playing, setPlaying] = useState(false);
+const videoRef = useRef(null);
+
+useEffect(() => {
+  window.addEventListener('scroll', debounce(handleScroll, 200))
+
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, [])
+
+const startVideo = () => {
+  videoRef.current.pause();
+  setPlaying(false);
+}
+
+const pauseVideo = () => {
+  videoRef.current.play();
+  setPlaying(true);
+}
+
+const handleScroll = (e) => {
+  if (playing) {
+    pauseVideo();
+  }
+}
+
+const handleVideoPress = () => {
+  if (playing) {
+    startVideo();
+  } else {
+    pauseVideo();
+  }
+};
+
+return (
+  <div className="video">
+    <video
+      onClick={handleVideoPress}
+      className="video__player"
+      loop
+      ref={videoRef}
+      src={url}
+    ></video>
+    </div>
+  );
+}
+
+export default Video
