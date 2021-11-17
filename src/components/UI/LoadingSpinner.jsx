2b5dfb0ed129cdc91271858ab9b9467761d75393
maab2198 <maab2198@gmail.com>
2021-11-17 02:51:44 +0200

    dev(prettier): install, setup, initial run prettier

--- src/components/UI/LoadingSpinner.jsx
+++ src/components/UI/LoadingSpinner.jsx
@@ -1,11 +1,11 @@
-import styles from "./LoadingSpinner.module.css";
+import styles from "./LoadingSpinner.module.css"
 
 const LoadingSpinner = () => {
   return (
     <div className={styles.spinner}>
       <div className={styles.spinner}></div>
     </div>
-  );
-};
+  )
+}
 
-export default LoadingSpinner;
+export default LoadingSpinner
