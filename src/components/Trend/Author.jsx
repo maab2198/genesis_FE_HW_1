5109b78fb57a4a39424441d4ae46da01df68003a
maab2198 <maab2198@gmail.com>
2022-01-05 20:26:07 +0200

    refactoring: rename css_class author_names => author__names

--- src/components/Trend/Author.jsx
+++ src/components/Trend/Author.jsx
@@ -9,7 +9,7 @@ import styles from "./Author.module.css"
 const Author = ({ author }) => (
   <div className={styles.avatar__container}>
     <Avatar size="small" link={author.name} src={author.avatar} />
-    <Link className={styles.author_names} to={`/user/${author.name}`} replace>
+    <Link className={styles.author__names} to={`/user/${author.name}`} replace>
       <h3>{author.name}</h3>
       <h4>{author.nickName}</h4>
     </Link>
